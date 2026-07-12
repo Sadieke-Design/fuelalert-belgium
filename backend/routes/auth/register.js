75582c7b5c6e5647de73f8e04be4e4866682c1c0
root <root@herinner.me>
2026-07-12 13:45:12 +0000

    Auto save 2026-07-12 13:45:12

--- backend/routes/auth/register.js
+++ backend/routes/auth/register.js
@@ -1,4 +1,5 @@
-const express = require("express");
+import express from "express";
+
 const router = express.Router();
 
 router.post("/", async (req, res) => {
@@ -8,4 +9,4 @@ router.post("/", async (req, res) => {
     });
 });
 
-module.exports = router;
+export default router;
