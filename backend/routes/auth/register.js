98637e3906fe9b48fa7e68a2cf6c8fba0e5f202a
root <root@herinner.me>
2026-07-12 13:47:00 +0000

    Auto save 2026-07-12 13:47:00

--- backend/routes/auth/register.js
+++ backend/routes/auth/register.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Register API online"
+    });
+});
+
 router.post("/", async (req, res) => {
     res.json({
         success: true,
