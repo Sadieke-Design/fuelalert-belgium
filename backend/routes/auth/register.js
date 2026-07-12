fbe3a43a2b7b04345a19eb5c922ef22bf39ee783
root <root@herinner.me>
2026-07-12 13:54:58 +0000

    Auto save 2026-07-12 13:54:58

--- backend/routes/auth/register.js
+++ backend/routes/auth/register.js
@@ -1,38 +1,34 @@
 import express from "express";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Register API online"
-    });
+  res.json({
+    success: true,
+    message: "Register API online",
+  });
 });
 
 router.post("/", async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    termsAccepted,
+    privacyAccepted,
+  } = req.body;
 
-    const {
-        first_name,
-        last_name,
-        email,
-        password,
-        termsAccepted,
-        privacyAccepted
-    } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log("Nieuwe registratie:", req.body);
+  console.log("Nieuwe registratie:", req.body);
 
-    res.json({
-        success: true,
-        message: "Registratie ontvangen",
-        received: {
-            first_name,
-            last_name,
-            email,
-            termsAccepted,
-            privacyAccepted
-        }
-    });
+  res.json({
+    success: true,
+    email,
+    hashedPassword
+});
 });
 
 export default router;
